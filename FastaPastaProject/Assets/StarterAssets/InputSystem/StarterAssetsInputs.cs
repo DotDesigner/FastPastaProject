006ec1856343cf24bd60801e28f8cf520505a220
KrystianHermaszewski <117281999+KrystianHermaszewski@users.noreply.github.com>
2024-01-05 13:15:54 +0100

    Slide

---
+++ FastaPastaProject/Assets/StarterAssets/InputSystem/StarterAssetsInputs.cs
@@ -0,0 +1,92 @@
+using UnityEngine;
+#if ENABLE_INPUT_SYSTEM
+using UnityEngine.InputSystem;
+#endif
+
+namespace StarterAssets
+{
+    public class StarterAssetsInputs : MonoBehaviour
+    {
+        [Header("Character Input Values")]
+        public Vector2 move;
+        public Vector2 look;
+        public bool jump;
+        public bool sprint;
+        public bool slide; // Add this for slide input
+
+        [Header("Movement Settings")]
+        public bool analogMovement;
+
+        [Header("Mouse Cursor Settings")]
+        public bool cursorLocked = true;
+        public bool cursorInputForLook = true;
+
+#if ENABLE_INPUT_SYSTEM
+        public void OnMove(InputValue value)
+        {
+            MoveInput(value.Get<Vector2>());
+        }
+
+        public void OnLook(InputValue value)
+        {
+            if (cursorInputForLook)
+            {
+                LookInput(value.Get<Vector2>());
+            }
+        }
+
+        public void OnJump(InputValue value)
+        {
+            JumpInput(value.isPressed);
+        }
+
+        public void OnSprint(InputValue value)
+        {
+            SprintInput(value.isPressed);
+        }
+
+        // Add this method for the slide input
+        public void OnSlide(InputValue value)
+        {
+            SlideInput(value.isPressed);
+        }
+#endif
+
+        public void MoveInput(Vector2 newMoveDirection)
+        {
+            move = newMoveDirection;
+        }
+
+        public void LookInput(Vector2 newLookDirection)
+        {
+            look = newLookDirection;
+        }
+
+        public void JumpInput(bool newJumpState)
+        {
+            jump = newJumpState;
+        }
+
+        public void SprintInput(bool newSprintState)
+        {
+            sprint = newSprintState;
+        }
+
+        // Add this method for the slide input
+        public void SlideInput(bool newSlideState)
+        {
+            slide = newSlideState;
+            Debug.Log("Slide input state changed: " + slide);
+        }
+
+        private void OnApplicationFocus(bool hasFocus)
+        {
+            SetCursorState(cursorLocked);
+        }
+
+        private void SetCursorState(bool newState)
+        {
+            Cursor.lockState = newState ? CursorLockMode.Locked : CursorLockMode.None;
+        }
+    }
+}
